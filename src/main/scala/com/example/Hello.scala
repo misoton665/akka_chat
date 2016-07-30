e7ca6c3724f78b122c31585117b3081079cc4a9d
misoton665 <misoton998@hotmail.co.jp>
2016-07-30 17:16:17 +0900

    Add logger for fail binding

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -1,10 +1,12 @@
 package com.example
 
 import akka.actor.ActorSystem
+import akka.event.Logging
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
+
 import scala.io.StdIn
 
 object Hello {
@@ -14,6 +16,8 @@ object Hello {
     // needed for the future flatMap/onComplete in the end
     implicit val executionContext = system.dispatcher
 
+    val logger = Logging(system, getClass)
+
     val route =
       path("hello") {
         get {
@@ -22,6 +26,10 @@ object Hello {
       }
 
     val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+
+    bindingFuture.onFailure {
+      case err: Exception => logger.error(err, "binding failure!")
+    }
 
     println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
     StdIn.readLine() // let it run until user presses return
