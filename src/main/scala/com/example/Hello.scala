0f7359af14ab2efa9e260464a3cc65cd4857b1ec
misoton665 <misoton998@hotmail.co.jp>
2016-07-31 18:25:32 +0900

    Change indent in Hello.scala

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -6,42 +6,50 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
+import com.typesafe.config.ConfigFactory
 
 import scala.io.StdIn
 
-object Hello {
-  def main(args: Array[String]): Unit = {
-    implicit val system = ActorSystem("my-system")
-    implicit val materializer = ActorMaterializer()
-    // needed for the future flatMap/onComplete in the end
-    implicit val executionContext = system.dispatcher
+object Hello extends App {
+  implicit val system = ActorSystem("my-system")
+  implicit val materializer = ActorMaterializer()
+  // needed for the future flatMap/onComplete in the end
+  implicit val executionContext = system.dispatcher
 
-    val logger = Logging(system, getClass)
+  val logger = Logging(system, getClass)
 
-    val route =
-      pathSingleSlash {
-        get {
-          complete(HttpResponse(entity = HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>")))
-        }
-      } ~
+  val config = ConfigFactory.load()
+
+  val route =
+    pathSingleSlash {
+      get {
+        complete(HttpResponse(entity = HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>")))
+      }
+    } ~
       path("echo") {
         get {
           complete {
             ""
           }
         }
+      } ~
+      path("conf") {
+        get {
+          complete {
+            config.getString("app.hoge.puyo")
+          }
+        }
       }
 
-    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+  val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
 
-    bindingFuture.onFailure {
-      case err: Exception => logger.error(err, "binding failure!")
-    }
-
-    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
-    StdIn.readLine() // let it run until user presses return
-    bindingFuture
-      .flatMap(_.unbind()) // trigger unbinding from the port
-      .onComplete(_ => system.terminate()) // and shutdown when done
+  bindingFuture.onFailure {
+    case err: Exception => logger.error(err, "binding failure!")
   }
+
+  println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+  StdIn.readLine() // let it run until user presses return
+  bindingFuture
+    .flatMap(_.unbind()) // trigger unbinding from the port
+    .onComplete(_ => system.terminate()) // and shutdown when done
 }
