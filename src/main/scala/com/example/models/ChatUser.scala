7f495bea780e6dcb2fad2e6446a55a619b413295
misoton665 <misoton998@hotmail.co.jp>
2016-08-01 17:02:38 +0900

    Add ChatUser model as DB Table

---
+++ src/main/scala/com/example/models/ChatUser.scala
@@ -0,0 +1,23 @@
+package com.example.models
+
+import org.joda.time.DateTime
+import skinny.orm.{Alias, SkinnyCRUDMapper}
+
+case class ChatUser(id: Long, name: Option[String], nickname: Option[String], bio: Option[String], signUpDate: DateTime)
+
+case object ChatUser extends SkinnyCRUDMapper[ChatUser] {
+
+  import scalikejdbc._
+
+  override lazy val tableName = "chat_user"
+  override lazy val defaultAlias: Alias[ChatUser] = createAlias("u")
+
+  override def extract(rs: WrappedResultSet, n: ResultName[ChatUser]): ChatUser =
+    ChatUser(
+      id = rs.long(n.id),
+      name = rs.stringOpt(n.name),
+      nickname = rs.stringOpt(n.nickname),
+      bio = rs.stringOpt(n.bio),
+      signUpDate = rs.jodaDateTime(n.signUpDate)
+    )
+}
