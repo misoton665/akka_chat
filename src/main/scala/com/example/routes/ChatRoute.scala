13004fe29a33ee7adbc2706d897f5cc089301a36
misoton665 <misoton998@hotmail.co.jp>
2016-08-14 09:16:31 +0900

    Create a class for web socket hander to separate from ChatRoute.scala

--- src/main/scala/com/example/routes/ChatRoute.scala
+++ src/main/scala/com/example/routes/ChatRoute.scala
@@ -1,44 +1,17 @@
 package com.example.routes
 
 import akka.actor.ActorSystem
-import akka.http.scaladsl.model.ws.{Message, TextMessage}
 import akka.http.scaladsl.server.Route
-import akka.stream.{FlowShape, Materializer, OverflowStrategy, SourceShape}
-import akka.stream.scaladsl.{Flow, GraphDSL, Merge, Source}
-import com.example.chat.ChatSystem
-import com.example.chat.ChatSystemMessages._
+import akka.stream.Materializer
 
 case class ChatRoute()(implicit actorSystem: ActorSystem, materializer: Materializer) extends RouteExtractable {
 
   import akka.http.scaladsl.server.Directives._
 
-  def handler(userId: String): Flow[Message, Message, _] = Flow.fromGraph(GraphDSL.create(Source.actorRef[SystemMessage](5, OverflowStrategy.fail)) {
-    implicit builder =>
-      chatSource =>
-        import GraphDSL.Implicits._
-        val fromWebsocket = builder.add(Flow[Message].collect {
-          case TextMessage.Strict(txt) =>
-            NewMessage(userId, MessageBody(txt))
-        })
-
-        val merge = builder.add(Merge[ChatSystemMessage](2))
-
-        val backToWebsocket = builder.add(Flow[SystemMessage].map[Message] { m => TextMessage.Strict(m.body) })
-
-        val actorAsSource = builder.materializedValue.map {
-          JoinedMessage(userId, _)
-        }
-
-        fromWebsocket ~> merge ~> chatSystem.chatLeftSink(userId)
-        actorAsSource ~> merge
-        chatSource ~> backToWebsocket
-        FlowShape(fromWebsocket.in, backToWebsocket.out)
-  })
-
-  val chatSystem = ChatSystem()
+  val chatStreamHandler = ChatStreamHandler()
 
   override def route: Route =
     (get & parameter('name)) { (userId) =>
-      handleWebSocketMessages(handler(userId))
+      handleWebSocketMessages(chatStreamHandler.handler(userId))
     }
 }
---
+++ src/main/scala/com/example/routes/ChatStreamHandler.scala
@@ -0,0 +1,36 @@
+package com.example.routes
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.model.ws.{Message, TextMessage}
+import akka.stream.{FlowShape, Materializer, OverflowStrategy}
+import akka.stream.scaladsl.{Flow, GraphDSL, Merge, Source}
+import com.example.chat.ChatSystem
+import com.example.chat.ChatSystemMessages._
+
+case class ChatStreamHandler()(implicit actorSystem: ActorSystem, materializer: Materializer) {
+
+  val chatSystem = ChatSystem()
+
+  def handler(userId: String): Flow[Message, Message, _] =
+    Flow.fromGraph(GraphDSL.create(Source.actorRef[SystemMessage](5, OverflowStrategy.fail)) {
+    implicit builder =>
+      chatSource =>
+        import GraphDSL.Implicits._
+        val fromWebsocket = builder.add(Flow[Message].collect {
+          case TextMessage.Strict(txt) => NewMessage(userId, MessageBody(txt))
+        })
+
+        val merge = builder.add(Merge[ChatSystemMessage](2))
+
+        val backToWebsocket = builder.add(Flow[SystemMessage].map[Message] { m => TextMessage.Strict(m.body) })
+
+        val actorAsSource = builder.materializedValue.map {
+          JoinedMessage(userId, _)
+        }
+
+        fromWebsocket ~> merge ~> chatSystem.chatLeftSink(userId)
+        actorAsSource ~> merge
+        chatSource ~> backToWebsocket
+        FlowShape(fromWebsocket.in, backToWebsocket.out)
+  })
+}
