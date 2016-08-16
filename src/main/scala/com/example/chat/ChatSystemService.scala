59b5070a7a2183cbe9ffe3381aecb8e6df90599f
misoton665 <misoton998@hotmail.co.jp>
2016-08-17 04:57:31 +0900

    Fix condition of signUp

--- src/main/scala/com/example/chat/ChatSystemService.scala
+++ src/main/scala/com/example/chat/ChatSystemService.scala
@@ -7,7 +7,7 @@ case class ChatSystemService()(implicit dBSession: DBSession) {
   private val chatUserService = ChatUserService()
 
   def signUp(userId: String, name: String): Option[Long] = {
-    if (chatUserService.exist(userId)) {
+    if (!chatUserService.exist(userId)) {
       val tableId = chatUserService.create(userId, name)
       Some(tableId)
     } else {
