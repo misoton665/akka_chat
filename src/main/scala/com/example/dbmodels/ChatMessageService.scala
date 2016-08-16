6b7604836142bae2eafa975e95842e817336212f
misoton665 <misoton998@hotmail.co.jp>
2016-08-17 05:09:32 +0900

    Add ChatMessageService

--- src/main/scala/com/example/dbmodels/ChatMessageService.scala
+++ src/main/scala/com/example/dbmodels/ChatMessageService.scala
@@ -1,11 +1,12 @@
 package com.example.dbmodels
 
 import org.joda.time.DateTime
+import scalikejdbc.DBSession
 import skinny.orm.{Alias, SkinnyCRUDMapper}
 
 case class ChatMessageRow(id: Long, userId: String, body: String, createAt: DateTime)
 
-case object ChatMessage extends SkinnyCRUDMapper[ChatMessageRow] {
+private case object ChatMessage extends SkinnyCRUDMapper[ChatMessageRow] {
 
   import scalikejdbc._
 
@@ -19,4 +20,21 @@ case object ChatMessage extends SkinnyCRUDMapper[ChatMessageRow] {
       body = rs.string(n.body),
       createAt = rs.jodaDateTime(n.createAt)
     )
+}
+
+case class ChatMessageService()(implicit dBSession: DBSession) {
+  val chatUserService = ChatUserService()
+
+  def add(userId: String, body: String): Option[Long] = {
+    if(chatUserService.exist(userId)) {
+      val id = ChatMessage.createWithAttributes('userId -> userId, 'body -> body)
+      Some(id)
+    } else {
+      None
+    }
+  }
+
+  def findAll(userId: String): List[ChatMessageRow] = {
+    ChatMessage.where('userId -> userId).apply()
+  }
 }
